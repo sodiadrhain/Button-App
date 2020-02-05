7e602ffe21a7c9b4e213fabb6814c89a58fb590f
sodiadrhain <adesojiawobajo@gmail.com>
2020-02-05 22:12:04 +0100

    Updates

---
+++ precache-manifest.6e8544d848697f9093453aae5b4b12ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a63c72b9220ecac1dd5186bf437ce55",
+    "url": "/button-app/index.html"
+  },
+  {
+    "revision": "3c8f96050bfb4e0efe7c",
+    "url": "/button-app/static/css/main.ba569edc.chunk.css"
+  },
+  {
+    "revision": "e1e3a01f0bd594995cda",
+    "url": "/button-app/static/js/2.d90c8d31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/button-app/static/js/2.d90c8d31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c8f96050bfb4e0efe7c",
+    "url": "/button-app/static/js/main.7016aefb.chunk.js"
+  },
+  {
+    "revision": "6ca954abddb9e302a3dc",
+    "url": "/button-app/static/js/runtime-main.f551c80d.js"
+  }
+]);
