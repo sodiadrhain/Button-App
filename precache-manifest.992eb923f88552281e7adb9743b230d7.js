6a181f210b30bddb77c5d62dc5d9cb15fab25daa
sodiadrhain <adesojiawobajo@gmail.com>
2020-02-06 07:40:27 +0100

    Updates

---
+++ precache-manifest.992eb923f88552281e7adb9743b230d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164066904446b87889a2246700650009",
+    "url": "/Button-app/index.html"
+  },
+  {
+    "revision": "02eaf7fe2e19dab615ba",
+    "url": "/Button-app/static/css/main.ba569edc.chunk.css"
+  },
+  {
+    "revision": "e1e3a01f0bd594995cda",
+    "url": "/Button-app/static/js/2.d90c8d31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Button-app/static/js/2.d90c8d31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02eaf7fe2e19dab615ba",
+    "url": "/Button-app/static/js/main.656f7bc8.chunk.js"
+  },
+  {
+    "revision": "ad6201acacb2fb3306ac",
+    "url": "/Button-app/static/js/runtime-main.902a2071.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/button-app/precache-manifest.6e8544d848697f9093453aae5b4b12ff.js"
+  "/Button-app/precache-manifest.992eb923f88552281e7adb9743b230d7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/button-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Button-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
